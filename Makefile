7f6a2a3ffb29daaab9c7d8ee7a3d1ea4ce9d4c2b
Hannes Schmidt <hannes@ucsc.edu>
2018-07-23 22:01:07 -0700

    Migrated prototype from Azul, added test and examples (fixes #1)

---
+++ Makefile
@@ -0,0 +1,18 @@
+SHELL=/bin/bash
+
+ifneq ($(shell python -c "import sys; print(hasattr(sys, 'real_prefix'))"),True)
+$(error Looks like no virtualenv is active)
+endif
+
+ifneq ($(shell python -c "import sys; print(sys.version_info >= (3,6))"),True)
+$(error Looks like Python 3.6 is not installed or active in the current virtualenv)
+endif
+
+install:
+	pip install -e .[helpers,examples]
+
+test: install
+	python -m unittest discover -s test
+
+examples: install
+	jupyter-notebook
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hca-metadata-api",
+    version="0.1",
+    license='MIT',
+    install_requires=[
+        'dataclasses >= 0.6'
+    ],
+    # Not using tests_require because that installs the test requirements into .eggs, not the virtualenv
+    extras_require={
+        "helpers": [
+            'hca == 4.1.0',
+            'urllib3 >= 1.23'
+        ],
+        "examples": [
+            'jupyter >= 1.0.0'
+        ]
+    },
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    project_urls={
+        "Source Code": "https://github.com/HumanCellAtlas/metadata-api",
+    }
+)
