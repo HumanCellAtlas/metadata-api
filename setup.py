71184f91517bacfb91d991c7dccb8b9e809fd1c3
Hannes Schmidt <hannes@ucsc.edu>
2018-08-06 21:33:20 -0700

    Prepare alpha 3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hca-metadata-api",
-    version="1.0a2",
+    version="1.0a3.dev1",
     license='MIT',
     install_requires=[
         'dataclasses >= 0.6'
