7a9d44e2048f6d351aca15ed87a9f2d7535b939a
Hannes Schmidt <hannes@ucsc.edu>
2018-09-24 19:06:08 -0700

    Fix: distribution is essentially empty
    Regression from a26cfbf48b2d39508db7c8314b56b944bee421b7

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="hca-metadata-api",
@@ -25,7 +25,7 @@ setup(
         ]
     },
     package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=find_namespace_packages('src'),
     project_urls={
         "Source Code": "https://github.com/HumanCellAtlas/metadata-api",
     }
