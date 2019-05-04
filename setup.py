2b96ae710f0df111ff7e7551476709d2567d0f07
Hannes Schmidt <hannes@ucsc.edu>
2019-05-03 20:39:25 -0700

    Fix: New paging in DSS `GET /bundles` causes KeyError (#73)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Not using tests_require because that installs the test requirements into .eggs, not the virtualenv
     extras_require={
         "dss": [
-            'hca == 4.4.1',
+            'hca == 5.2.0',
             'urllib3 >= 1.23'
         ],
         "examples": [
